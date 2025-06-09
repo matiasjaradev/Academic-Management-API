68ce64b99704b7e34d424c54e4a11b123d426935
Matias jara <matiasjats@gmail.com>
2025-06-09 19:17:08 -0400

    add rout subject

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
-from routers import user, security,studients
+from routers import user, security,studients, subjects
 
 app = FastAPI()
 
 app.include_router(user.router)
 app.include_router(security.router)
 app.include_router(studients.router)
+app.include_router(subjects.router)
